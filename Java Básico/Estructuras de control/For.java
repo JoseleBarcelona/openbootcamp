6df4142b68a119c8f7e8962c7257fc270c98e3a7
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-02-25 17:10:17 +0100

    Add files via upload

---
+++ Java Básico/Estructuras de control/For.java
@@ -0,0 +1,17 @@
+package CursoJavaBasico.EstructurasControl.Repeticion;
+
+public class For {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <10; i++){
+            System.out.println("El valor de i es: " + i );
+
+        }
+        for (int k = 0; k < 10; k+=2){
+            System.out.println("El valor de j se incrementa en 2: " + k );
+        }
+
+
+
+    }
+}
