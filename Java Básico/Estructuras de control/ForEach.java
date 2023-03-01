78e92aac915aa7e4db2d6f70237b61c961513e05
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-03-01 14:12:33 +0100

    Add files via upload

---
+++ Java Básico/Estructuras de control/ForEach.java
@@ -0,0 +1,21 @@
+package CursoJavaBasico.EstructurasControl.Repeticion;
+
+public class ForEach {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Papa", "Mama", "Neus"};
+        for (String nombre : nombres){
+            System.out.println(nombre);
+        }
+        int[] numeros = {5, 10, 15};
+        for (int numero : numeros){
+            System.out.println(numero);
+        }
+        int suma = 0;
+        int[] masnumeros = {10, 20, 30};
+        for (int masnumero : masnumeros){
+            suma += masnumero;
+            System.out.println(suma);
+        }
+    }
+}
