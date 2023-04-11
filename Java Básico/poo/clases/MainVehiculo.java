ba8095ab4f544b8d0139a473173840dd00e35bce
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-04-11 20:29:42 +0200

    Add files via upload

--- Java Básico/poo/clases/MainVehiculo.java
+++ Java Básico/poo/clases/MainVehiculo.java
@@ -1,11 +1,14 @@
 package CursosBootcamp.poo.clases;
 
+import CursosBootcamp.poo.Herencia.Camion;
+import CursosBootcamp.poo.Herencia.Coche;
 import CursosBootcamp.poo.Herencia.Motocicleta;
 
 public class MainVehiculo {
     public static void main(String[] args) {
 
-        //Creación de objetos
+        //1. Creación de objetos
+
         //Clase identificador = new Constructor();
 
         //Con el objeto ToyotaPrius vamos a utilizar el constructor vacío
@@ -30,11 +33,29 @@ public class MainVehiculo {
         FordMondeo.acelerar(50);      //Velocidad pasada llamando al método acelerar
         System.out.println(FordMondeo.speed); //Velocidad adquirida después de recibir la velocidad
 
+        //2. Herencia
+
         Motocicleta kawasakiNinja = new Motocicleta();
         kawasakiNinja.fabricante = "Kawasaki";
         System.out.println(kawasakiNinja.fabricante);
 
+        //3. Polimorfismo
 
+
+        Vehiculo vehiculo; //Declaramos una variable de la clase padre para crear objetos de las clases hijas
+
+        vehiculo = new Motocicleta();  //Usamos la clase padre con los constructores y métodos de Motocicleta
+        vehiculo.acelerar(10);  //llamamos al método acelerar de la clase padre y se guarda en Motocicleta
+
+        vehiculo = new Camion();
+        vehiculo.acelerar(20);
+
+        vehiculo = new Coche();
+        vehiculo.acelerar(50);
+
+        //4. Clases abstractas: public abstract class Vehiculo {}
+        //No se pueden instanciar (No se pueden crear objetos de esas clases)
+        //Sólo pueden ser instanciadas sus clases hijas (Sí se pueden crear objetos)
 
     }
 }
