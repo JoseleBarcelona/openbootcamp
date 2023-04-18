3afc5df728fe2782f96e919149a8ac45ed649213
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-04-18 19:47:40 +0200

    Delete Java Básico/TiposDatosAvanzados/String directory

--- Java Básico/TiposDatosAvanzados/String/StringcharAt.java
+++
@@ -1,9 +0,0 @@
-package CursosBootcamp.TiposDatosAvanzados.String;
-
-public class StringcharAt {
-    public static void main(String[] args) {
-        String cadena = "MenSaJE de TEXTo";
-
-        System.out.println("El carácter ubicado en el índice 6 es: " + cadena.charAt(6));
-    }
-}
