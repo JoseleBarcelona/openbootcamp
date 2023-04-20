09dacd2f35b5d89ae8a585a4e9045aea8f957281
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-04-20 12:26:40 +0200

    Add files via upload

---
+++ Java Básico/IntroduccionProgramacion/EjercicioTema3.java
@@ -0,0 +1,28 @@
+package IntroduccionProgramacion;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+
+        int resultado = suma(5,10,15); //Invocamos a la función y la guardamos en una variable para mostrar en consola
+        System.out.println("\nLa suma de a, b, y c es de: " + resultado);
+
+        Coche miCoche = new Coche();
+
+        miCoche.añadirPuertas();
+
+        System.out.println("\nEl coche tenía 4 puertas");
+        System.out.println("\nAhora tiene " + miCoche.numeroDePuertas + " puertas");
+    }
+    public static int suma(int a, int b, int c){
+        return a +b +c; //Esto devuelve unos datos que para poder utilizarlos, hay que guardarlos en una variable
+    }
+
+}
+class Coche{
+    public int numeroDePuertas = 4;
+
+    public void añadirPuertas(){
+        this.numeroDePuertas++;
+    }
+
+}
