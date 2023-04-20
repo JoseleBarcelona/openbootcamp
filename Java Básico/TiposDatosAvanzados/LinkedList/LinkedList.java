ddbb014179bbb9a964dbf4bb040fdd4d9818b76f
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-04-20 22:37:32 +0200

    Add files via upload

---
+++ Java Básico/TiposDatosAvanzados/LinkedList/LinkedList.java
@@ -0,0 +1,17 @@
+package CursosBootcamp.JavaBasico.TiposDatosAvanzados.LinkedList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+        java.util.LinkedList<String> listaEnlazada = new java.util.LinkedList<>();
+         /*
+         Una LinkedList se declara igual que una ArrayList.
+         Hay dos grandes diferencias entre ellas:
+         1. Una ArrayList utiliza una array de manera subyacente (por debajo), en cambio una
+         LinkedList utiliza una estructura de datos llamada lista de doblemente enlazada.
+         2. El ArrayList es mucho más rápido para buscar y almacenar datos, en cambio una
+         LinkedList es mucho más rápida a la hora de modificar datos.
+
+          */
+    }
+}
