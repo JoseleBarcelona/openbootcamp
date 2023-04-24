e68f4848c715c3fe117c327b31697e0d02ca13d3
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-04-24 09:11:20 +0200

    Add files via upload

--- Java Básico/TiposDatosAvanzados/String/StringcharAt.java
+++ Java Básico/TiposDatosAvanzados/String/StringcharAt.java
@@ -1,9 +1,17 @@
-package CursosBootcamp.TiposDatosAvanzados.String;
+package CursosBootcamp.JavaBasico.TiposDatosAvanzados.String;
+
+import java.util.List;
 
 public class StringcharAt {
     public static void main(String[] args) {
         String cadena = "MenSaJE de TEXTo";
 
         System.out.println("El carácter ubicado en el índice 6 es: " + cadena.charAt(6));
+
     }
 }
+/*
+Código ejecutado
+
+El carácter ubicado en el índice 6 es: E
+ */
