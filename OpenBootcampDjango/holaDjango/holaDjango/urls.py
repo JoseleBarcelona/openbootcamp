485530c357b3d7f48a3df6eb8f7585b105bdea32
Josele Barcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-08-11 19:05:11 +0200

    Delete OpenBootcampDjango directory

--- OpenBootcampDjango/holaDjango/holaDjango/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('saludo/', views.saludo, name='saludo'),
-    path('despedida/', views.despedida, name='despedida'),
-    path('adulto/<int:edad>/', views.adulto, name='adulto'),
-]
--- OpenBootcampDjango/plantillas/plantillas/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def simple(request):
-    return render(request, 'simple.html', {})
-
-def dinamico(request, name):
-    categorias= ['código', 'diseño', 'marketing']
-    contexto= {'name':name, 'categorias':categorias} #El contexto siempre es un diccionario con clave-valor
-    return render(request, 'dinamico.html', contexto)
